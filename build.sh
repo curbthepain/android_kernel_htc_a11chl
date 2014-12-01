3af6ceaffefbd8e52dbe08937245fdd4048b91dd
Czar Dixon <rbheromax@gmail.com>
2014-12-01 17:34:07 -0500

    Link build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export ARCH=arm
 export CROSS_COMPILE=../arm-eabi-4.6/bin/arm-eabi-
-# make clean
-# make a11ul_defconfig
+make clean
+make a11chl_defconfig
 make all CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
+bin/make.sh
+echo "Image is at:"
+echo "boot.img"
