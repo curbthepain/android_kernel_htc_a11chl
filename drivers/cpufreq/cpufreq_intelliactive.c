7d56af3c51b37e8ab87a009c1e7684f992d6e98a
Austin Wisniewski <curb.the.pain@gmail.com>
2014-12-09 09:52:47 -0600

    Random edit.

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -1549,3 +1549,4 @@ MODULE_AUTHOR("Paul Reioux <reioux@gmail.com>");
 MODULE_DESCRIPTION("'cpufreq_intelliactive' - A cpufreq governor for "
 	"Latency sensitive workloads based on Google's Interactive");
 MODULE_LICENSE("GPL");
+
